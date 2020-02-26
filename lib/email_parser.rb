d87d5df5d3f39619ae89e4e4cac293a0e4ec70db
Keanna Salter <keannasalter91@gmail.com>
2020-02-26 23:19:13 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,1 +1,16 @@
+class EmailParser
 
+  attr_accessor :emails
+
+  def initialize(emails)
+    @emails = emails
+  end
+
+  def parse
+    email_array = []
+    email_array = @emails.split(/[,\s]/)
+    email_array = email_array.reject { |e| e == ""}
+    email_array.uniq
+  end
+
+end
